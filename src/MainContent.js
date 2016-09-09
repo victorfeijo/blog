1f122b14c7ca38dfb6fa1406ccbcb1810e9d8ac5
Victor Feijo <victor.feijoa@gmail.com>
2016-09-09 19:03:04 -0300

    init repository

---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './MainContent.css'
+
+class MainContent extends Component {
+  render() {
+    return (
+      <div>
+        <a> I am the main content </a>
+      </div>
+    );
+  }
+}
+
+export default MainContent
---
+++ src/sidemenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SideMenu extends Component {
+  render() {
+    return (
+      <div>
+        <a> I am the side menu </a>
+      </div>
+    );
+  }
+}
+
+export default SideMenu
